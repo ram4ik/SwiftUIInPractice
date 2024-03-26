c8b611268bc61bb1adb111c2960edcabd62efc53
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-03-26 19:54:27 +0200

    Initial Commit

---
+++ SwiftUIInPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIInPractice
+//
+//  Created by Ramill Ibragimov on 3/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIInPractice/SwiftUIInPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIInPracticeApp.swift
+//  SwiftUIInPractice
+//
+//  Created by Ramill Ibragimov on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIInPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
