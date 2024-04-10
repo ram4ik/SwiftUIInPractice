cb422997c7c42abd33d8aee80b0fb44b43419855
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-04-10 21:29:33 +0300

    Rebuild Netflix in SwiftUI part 3

---
+++ SwiftUIInPractice/Netflix/Components/NetflixMovieCell.swift
@@ -0,0 +1,18 @@
+//
+//  NetflixMovieCell.swift
+//  SwiftUIInPractice
+//
+//  Created by Ramill Ibragimov on 4/10/24.
+//
+
+import SwiftUI
+
+struct NetflixMovieCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetflixMovieCell()
+}
